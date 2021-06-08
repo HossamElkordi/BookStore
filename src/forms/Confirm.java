e1d498885e3b0cbe4f259958fdbbc1a8514e4318
ahm_e <ahm0ed0yasser@gmail.com>
2021-06-08 18:12:14 +0200

    added confirm panel

--- src/forms/Confirm.java
+++ src/forms/Confirm.java
@@ -6,37 +6,54 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Confirm extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTable table;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Confirm frame = new Confirm();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public Confirm() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 518, 441);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
 
+		textField = new JTextField();
+		textField.setBounds(39, 58, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+
+		JLabel lblNewLabel = new JLabel("ISBN");
+		lblNewLabel.setBounds(39, 35, 46, 14);
+		contentPane.add(lblNewLabel);
+
+		JButton btnNewButton = new JButton("Search");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(180, 57, 89, 23);
+		contentPane.add(btnNewButton);
+
+		table = new JTable();
+		table.setBounds(10, 115, 482, 43);
+		contentPane.add(table);
+
+		JButton btnNewButton_1 = new JButton("Confirm");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(180, 197, 89, 23);
+		contentPane.add(btnNewButton_1);
+	}
 }
