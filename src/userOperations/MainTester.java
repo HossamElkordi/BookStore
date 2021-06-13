bc42c617bc0728aa544c650e53584ca25b330eb9
Hossam Elkordi <hossam_elkordi@yahoo.com>
2021-06-13 19:37:33 +0200

    fixes

--- src/userOperations/MainTester.java
+++ src/userOperations/MainTester.java
@@ -5,6 +5,5 @@ import java.util.ArrayList;
 public class MainTester {
     public static void main(String[] args) {
         Operation op = new AddNewBooks();
-        op.execute(new ArrayList<String>());
     }
 }
