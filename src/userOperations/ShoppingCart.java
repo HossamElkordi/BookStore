8476c895d113a2ebefca43e39c693006291e7816
Ramez Maher <ramezmaher@hotmail.com>
2021-06-14 07:55:13 +0200

    added observer relation to transactions

--- src/userOperations/ShoppingCart.java
+++ src/userOperations/ShoppingCart.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class ShoppingCart {
     private List<Book> Cart;
 
@@ -25,6 +26,9 @@ public class ShoppingCart {
     public void Checkout(LogIn User)
     {
         Operation operation = new ModifyExistingBook();
+
+        Sales sa = new Sales();
+
         for(Book book: this.Cart)
         {
             ArrayList<String> temp = new ArrayList<>();
@@ -32,6 +36,13 @@ public class ShoppingCart {
             temp.add("Quantity= Quantity-"+book.getQuantity());
             temp.add("ISBN="+book.getISBN());
             operation.execute(temp);
+
+            ArrayList<String> list = new ArrayList<>();
+            list.add(User.getUserName());
+            list.add(book.getISBN());
+            list.add(java.time.LocalDate.now().toString());
+            list.add(book.getPrice());
+            sa.execute(list);
         }
         clear();
     }
